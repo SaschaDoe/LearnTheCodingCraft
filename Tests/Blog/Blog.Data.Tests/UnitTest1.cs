130035bcdce2185761a96e1b84708cbf99c23def
Sascha Dörflein <sascha.doerflein@infoteam.de>
2021-11-24 17:59:57 +0100

    Rerange solution Add Blog.Data and test project

---
+++ Tests/Blog/Blog.Data.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blog.Data.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
